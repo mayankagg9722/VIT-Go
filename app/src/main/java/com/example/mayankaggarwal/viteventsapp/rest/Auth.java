a13629fbb96610c37137a9415f6d98b4712c7739
mayankagg9722 <mayankagg9722@gmail.com>
2017-02-12 16:46:46 +0530

    added nointerner connectin

--- app/src/main/java/com/example/mayankaggarwal/viteventsapp/rest/Auth.java
+++ app/src/main/java/com/example/mayankaggarwal/viteventsapp/rest/Auth.java
@@ -3,18 +3,17 @@ package com.example.mayankaggarwal.viteventsapp.rest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.mayankaggarwal.viteventsapp.R;
 import com.example.mayankaggarwal.viteventsapp.models.LoginRequest;
 import com.example.mayankaggarwal.viteventsapp.models.LoginResponse;
+import com.example.mayankaggarwal.viteventsapp.utils.InternetConnection;
 import com.example.mayankaggarwal.viteventsapp.utils.Prefs;
 
 import retrofit2.Call;
@@ -29,60 +28,82 @@ public class Auth {
 
     public static void login(String regno, String password, final Activity activity, final OnLoginCallback onLoginCallback) {
 
+        if (!InternetConnection.isNetworkAvailable()) {
+            Log.d("called", "callde callde callde callde ");
+            final AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+            LayoutInflater inflater = activity.getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.error_window, null);
+            ImageView internetimage = (ImageView) dialogView.findViewById(R.id.errorImage);
+            internetimage.setImageResource(R.drawable.nointerneterrorbackground);
+            ImageButton ok = (ImageButton) dialogView.findViewById(R.id.errorImageOkButton);
+            alert.setView(dialogView);
+            final AlertDialog alertDialog = alert.create();
 
-        ApiInterface apiInterface = new ApiClient().getClient(activity).create(ApiInterface.class);
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("click", "ckicking ckicking ckicking ckicking");
+                    alertDialog.dismiss();
+                }
+            });
+            alertDialog.show();
+            onLoginCallback.onFailure();
+        } else {
 
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.regno = regno;
-        loginRequest.password = password;
+            ApiInterface apiInterface = new ApiClient().getClient(activity).create(ApiInterface.class);
+
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.regno = regno;
+            loginRequest.password = password;
 
 
-        Call<LoginResponse> login = apiInterface.login(loginRequest);
+            Call<LoginResponse> login = apiInterface.login(loginRequest);
 
-        login.enqueue(new Callback<LoginResponse>() {
-            @Override
-            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+            login.enqueue(new Callback<LoginResponse>() {
+                @Override
+                public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
 
-                try {
-                    if (response.body() != null) {
-                        if (response.body().code.equals("200")) {
-                            Prefs.setPrefs("loggedIn","true", activity);
-                            Prefs.setPrefs("Name", response.body().message.toString(), activity);
-                            onLoginCallback.onSuccess();
+                    try {
+                        if (response.body() != null) {
+                            if (response.body().code.equals("200")) {
+                                Prefs.setPrefs("loggedIn", "true", activity);
+                                Prefs.setPrefs("Name", response.body().message.toString(), activity);
+                                onLoginCallback.onSuccess();
+                            } else {
+                                final AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+                                LayoutInflater inflater = activity.getLayoutInflater();
+                                final View dialogView = inflater.inflate(R.layout.error_window, null);
+                                ImageButton ok = (ImageButton) dialogView.findViewById(R.id.errorImageOkButton);
+                                alert.setView(dialogView);
+                                final AlertDialog alertDialog = alert.create();
+
+                                ok.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Log.d("click", "ckicking ckicking ckicking ckicking");
+                                        alertDialog.dismiss();
+                                    }
+                                });
+                                alertDialog.show();
+                                onLoginCallback.onFailure();
+                            }
                         } else {
-                            final AlertDialog.Builder alert=new AlertDialog.Builder(activity);
-                            LayoutInflater inflater = activity.getLayoutInflater();
-                            final View dialogView = inflater.inflate(R.layout.error_window, null);
-                            ImageButton ok= (ImageButton) dialogView.findViewById(R.id.errorImageOkButton);
-                            alert.setView(dialogView);
-                            final AlertDialog alertDialog=alert.create();
-
-                            ok.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Log.d("click","ckicking ckicking ckicking ckicking");
-                                    alertDialog.dismiss();
-                                }
-                            });
-                            alertDialog.show();
                             onLoginCallback.onFailure();
                         }
-                    } else {
+                    } catch (Exception e) {
+                        e.printStackTrace();
                         onLoginCallback.onFailure();
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    onLoginCallback.onFailure();
+
                 }
 
-            }
+                @Override
+                public void onFailure(Call<LoginResponse> call, Throwable t) {
+                    onLoginCallback.onFailure();
+                }
+            });
 
-            @Override
-            public void onFailure(Call<LoginResponse> call, Throwable t) {
-                onLoginCallback.onFailure();
-            }
-        });
-
+        }
     }
 
 
