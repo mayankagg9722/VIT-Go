12552319e3175cffe0800c97907dc02ec97b51c7
mayankagg9722 <mayankagg9722@gmail.com>
2017-03-26 19:30:07 +0530

    added marks

--- app/src/main/java/com/example/mayankaggarwal/viteventsapp/adapter/RVMarks.java
+++ app/src/main/java/com/example/mayankaggarwal/viteventsapp/adapter/RVMarks.java
@@ -3,20 +3,31 @@ package com.example.mayankaggarwal.viteventsapp.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.mayankaggarwal.viteventsapp.R;
 import com.example.mayankaggarwal.viteventsapp.utils.SetTheme;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by mayankaggarwal on 26/03/17.
@@ -33,7 +44,7 @@ public class RVMarks extends RecyclerView.Adapter<RVMarks.MyViewHolder> {
         this.context=context;
         parser=new JsonParser();
         jsonArray=parser.parse(marks).getAsJsonObject().get("data").getAsJsonArray();
-        Log.d("tagg","ppp:"+jsonArray.get(0).getAsJsonObject().get("Course Title").getAsString());
+//        Log.d("tagg","ppp:"+jsonArray.get(0).getAsJsonObject().get("Course Title").getAsString());
     }
 
     @Override
@@ -55,7 +66,27 @@ public class RVMarks extends RecyclerView.Adapter<RVMarks.MyViewHolder> {
         holder.course_name.setText(object.get("Course Title").getAsString());
         holder.coursecode.setText(object.get("Course Code").getAsString()+" - "+object.get("Faculty").getAsString());
         holder.facultyname.setText(object.get("Faculty").getAsString());
-//        holder.layoutMarks.setJsonData(marks);
+        addTextFields(marks,holder,holder.customMarksLayout);
+    }
+
+    private void addTextFields(JsonObject marks,RVMarks.MyViewHolder holder,LinearLayout customMarksLayout) {
+
+        Set<Map.Entry<String, JsonElement>> entrySet = marks.entrySet();
+        for(Map.Entry<String,JsonElement> entry : entrySet){
+
+            Log.d("tagg",entry.getKey()+": "+entry.getValue());
+            TextInputLayout textInputLayout = new TextInputLayout(context);
+            TextView name = new TextView(context);
+//            name.getBackground().mutate().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP);
+//            name.setHint(s);
+//            name.setId(i);
+            name.setText(entry.getKey()+" : "+entry.getValue().getAsString()+"\n");
+            textInputLayout.addView(name);
+            name.setTextColor(Color.parseColor("#ffffff"));
+            name.setTextSize(15f);
+            holder.customMarksLayout.addView(textInputLayout);
+        }
+
     }
 
     @Override
@@ -66,6 +97,7 @@ public class RVMarks extends RecyclerView.Adapter<RVMarks.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView course_name,coursecode,facultyname;
         CardView cardView;
+        LinearLayout customMarksLayout;
 
 //        public CustomLayoutMarks layoutMarks=new CustomLayoutMarks(context);
 
@@ -76,8 +108,8 @@ public class RVMarks extends RecyclerView.Adapter<RVMarks.MyViewHolder> {
             course_name=(TextView)itemView.findViewById(R.id.course_name_marks);
             coursecode=(TextView)itemView.findViewById(R.id.coursecodeandtype);
             facultyname=(TextView)itemView.findViewById(R.id.facultynamemarks);
-//            layoutMarks=(com.example.mayankaggarwal.viteventsapp.activities.CustomLayoutMarks)itemView.findViewById(R.id.customMarks);
             cardView.setCardBackgroundColor(Color.parseColor(SetTheme.colorName));
+            customMarksLayout=(LinearLayout)itemView.findViewById(R.id.marskcustomlayout);
 
         }
     }
